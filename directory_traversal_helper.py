6624653bab44ca4c66592ab0678340c1bdd07174
Greg D. Hill <greghill@cs.stanford.edu>
2016-01-31 22:44:25 -0800

    undo excess printout

--- directory_traversal_helper.py
+++ directory_traversal_helper.py
@@ -11,6 +11,4 @@ def get_files_matching_regex(directory_path, regex):
                 # if regex has no subgroups use file name as identifier
                 identifier = f if len(match_object.groups()) is 0 else match_object.group(1)
                 ret.append((filepath, identifier))
-            else:
-                print("no match for " + f)
     return ret
