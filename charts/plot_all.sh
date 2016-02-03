82d3ab404e3dcb108b60b713ec03e3bc6ebba7de
Greg D. Hill <greghill@cs.stanford.edu>
2016-02-02 19:12:27 -0800

    parallelize plotting

--- charts/plot_all.sh
+++ charts/plot_all.sh
@@ -3,12 +3,13 @@ for d in */datapoints
 do
     dataset=`echo $d | sed 's/\/datapoints//g'`
 
-    gnuplot -e "dataset='$dataset'" gnuplot_scripts/inter-frame-delays-ccdf.plt
-    gnuplot -e "dataset='$dataset'" gnuplot_scripts/inverse-complement-ssim-cdf.plt
-    gnuplot -e "dataset='$dataset'" gnuplot_scripts/min-ssim-cdf.plt
-    gnuplot -e "dataset='$dataset'" gnuplot_scripts/proportional-playback.plt
-    gnuplot -e "dataset='$dataset'" gnuplot_scripts/rebuffering-ratios-cdf.plt
-    gnuplot -e "dataset='$dataset'" gnuplot_scripts/resume-delays-cdf.plt
-    gnuplot -e "dataset='$dataset'" gnuplot_scripts/ssim-cdf.plt
-    echo "Finished plotting $dataset
+    gnuplot -e "dataset='$dataset'" gnuplot_scripts/inter-frame-delays-ccdf.plt &
+    gnuplot -e "dataset='$dataset'" gnuplot_scripts/inverse-complement-ssim-cdf.plt &
+    gnuplot -e "dataset='$dataset'" gnuplot_scripts/proportional-playback.plt &
+    gnuplot -e "dataset='$dataset'" gnuplot_scripts/rebuffering-ratios-cdf.plt &
+    gnuplot -e "dataset='$dataset'" gnuplot_scripts/resume-delays-cdf.plt &
+    gnuplot -e "dataset='$dataset'" gnuplot_scripts/ssim-cdf.plt &
+    gnuplot -e "dataset='$dataset'" gnuplot_scripts/min-ssim-cdf.plt &
+    wait
+    echo Finished plotting $dataset
 done
